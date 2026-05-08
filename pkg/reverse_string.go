c40f6388919deb5b2e608983cdae4f5a4571d3cd
Cording Pilot <cording-pilot@example.com>
2026-05-08 23:56:24 +0900

    feat: Issue #46 の実装

---
+++ pkg/reverse_string.go
@@ -0,0 +1,10 @@
+package pkg
+
+// ReverseString returns the input string in reverse order by rune.
+func ReverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
